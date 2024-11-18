762c006475618b00fdd43814872ebf67e8ab5a52
Aydin Ghane Kh. <aydin.ghane.kh@gmail.com>
2024-11-18 19:01:07 +0100

    Add upload functionality

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -4,21 +4,45 @@ import UploadIcon from "../../assets/icons/upload.svg";
 import BrushIcon from "../../assets/icons/brush.svg";
 import TextIcon from "../../assets/icons/text.svg";
 import { useTranslation } from "react-i18next";
+import { useCallback } from "react";
+import Dropzone from "react-dropzone";
 
 export const Navigation = () => {
   const { t } = useTranslation();
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+
+      reader.onabort = () => console.log("file reading was aborted");
+      reader.onerror = () => console.log("file reading has failed");
+      reader.onload = () => {
+        // Do whatever you want with the file contents
+        const binaryStr = reader.result;
+        console.log(binaryStr);
+      };
+      reader.readAsDataURL(file);
+    });
+  }, []);
 
   return (
     <nav className={styles.container}>
       <ul>
         <li>
-          <Icon
-            text={t("navigation.upload")}
-            animated
-            onClick={() => console.log("upload")}
-          >
-            <UploadIcon />
-          </Icon>
+          <Dropzone onDrop={onDrop}>
+            {({ getRootProps, getInputProps }) => (
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <Icon
+                  text={t("navigation.upload")}
+                  animated
+                  onClick={() => console.log("upload")}
+                >
+                  <UploadIcon />
+                </Icon>
+              </div>
+            )}
+          </Dropzone>
         </li>
         <li>
           <Icon
