5a743ad7f98afd565536c9f04a8b4f1f87d3d758
Aydin Ghane Kh. <aydin.ghane.kh@gmail.com>
2024-11-23 20:09:34 +0100

    Minor CSS tweaks

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./localization/";
 
 import "./styles/reset.scss";
+import "./styles/scrollbar.scss";
 import "./styles/fonts.scss";
 import "./styles/vars.scss";
 import "./styles/main.scss";
